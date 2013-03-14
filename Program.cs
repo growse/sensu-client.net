ecd58b1a54f5f3b7087e6cfb917c77f25fb53010
Andrew Rowson <growse@github.com>
2013-03-14 09:45:00 +0000

    Initial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sensu_client.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
