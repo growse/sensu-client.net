6dc65677f7eab1bc77472e497ca6399c301a649c
Andrew Rowson <growse@github.com>
2013-04-16 13:45:50 +0100

    Generic configs

--- plugins/check_service.ps1
+++
@@ -1,9 +0,0 @@
-param([string]$service)
-$_service = get-service -ErrorAction Ignore -Name $service
-if ($_service.status -ne 'Running') {
-    echo "CRITICAL - $service is not running"
-    [Environment]::Exit(2)
-} else {
-    echo 'OK'
-    exit 0
-}
